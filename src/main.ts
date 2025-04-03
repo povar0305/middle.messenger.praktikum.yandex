fe2a6071d144dce425a5d66524e014c8c977394f
Sofia.Povarnitsyna <spo@item.expert>
2025-04-03 20:22:17 +0300

    шаблонизатор + компонент кнопки

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,1 @@
 import './style/main.sass'
-
-import Handlebars from 'handlebars';
-
-import templateBtn from './templates/btn.hbs';
-// Данные для рендеринга
-const data = {
-  text: 'Войти'
-};
-
-const template = Handlebars.compile(templateBtn);
-
-const html = template(data);
-
-document.body.innerHTML = html;
-
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import handlebars from 'vite-plugin-handlebars';
+import { resolve } from "path";
+
+export default defineConfig({
+  base: '/',
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'src/partials')
+    }),
+  ],
+  assetsInclude: ['**/*.html'], // Добавьте эту строку для обработки HTML файлов
+})
